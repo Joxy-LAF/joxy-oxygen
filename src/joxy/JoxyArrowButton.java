3a0b079187743ead6d50ed1b4ecc716462ea776c
Willem Sonke <willem3141@users.sourceforge.net>
2012-03-11 11:30:27 +0100

    Painting the arrow button

--- src/joxy/JoxyArrowButton.java
+++ src/joxy/JoxyArrowButton.java
@@ -1,10 +1,28 @@
 package joxy;
 
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.Line2D;
+import java.awt.geom.Rectangle2D;
+
 import javax.swing.plaf.basic.BasicArrowButton;
 
 public class JoxyArrowButton extends BasicArrowButton {
 
 	public JoxyArrowButton(int direction) {
 		super(direction);
+	}
+	
+	@Override
+	public void paint(Graphics g) {
+		Graphics2D g2 = (Graphics2D) g;
+		
+		Rectangle2D paintRect = new Rectangle2D.Float(6, 6, getWidth() - 12, getHeight() - 12);
+		
+		switch (direction) {
+		case SOUTH: default:
+			g2.draw(new Line2D.Double(paintRect.getMinX(), paintRect.getMinY(), paintRect.getCenterX(), paintRect.getMaxY()));
+			g2.draw(new Line2D.Double(paintRect.getMaxX(), paintRect.getMinY(), paintRect.getCenterX(), paintRect.getMaxY()));
+		}
 	}
 }
