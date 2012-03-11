24a05fb20b5f9d69137006a4441682ff6097c422
Willem Sonke <willem3141@users.sourceforge.net>
2012-03-11 11:41:27 +0100

    Further work on the arrow button

--- src/joxy/JoxyArrowButton.java
+++ src/joxy/JoxyArrowButton.java
@@ -1,8 +1,10 @@
 package joxy;
 
+import java.awt.BasicStroke;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.geom.Line2D;
+import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
 
 import javax.swing.plaf.basic.BasicArrowButton;
@@ -17,7 +19,14 @@ public class JoxyArrowButton extends BasicArrowButton {
 	public void paint(Graphics g) {
 		Graphics2D g2 = (Graphics2D) g;
 		
-		Rectangle2D paintRect = new Rectangle2D.Float(6, 6, getWidth() - 12, getHeight() - 12);
+		Point2D center = new Point2D.Double(getWidth() / 2f, getHeight() / 2f);
+		
+		double width = 10;
+		double height = 5;   // note: should be width / 2
+		
+		Rectangle2D paintRect = new Rectangle2D.Double(center.getX() - width / 2, center.getY() - height / 2, width, height);
+		
+		g2.setStroke(new BasicStroke(2));
 		
 		switch (direction) {
 		case SOUTH: default:
