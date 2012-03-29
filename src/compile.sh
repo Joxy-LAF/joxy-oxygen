70445aa42b8f516e983246c03bf66d02bc770ed8
Thom Castermans <thomcastermans@users.sourceforge.net>
2012-03-29 09:46:02 +0200

    Modified compile script to detect multiple Java installations and let the user choose between them (6 or 7). Remove some unused imports from TestGUI.

--- src/compile.sh
+++ src/compile.sh
@@ -5,10 +5,11 @@ NM=`basename $0`
 
 # Read params
 VERBOSE=0
+G_VERBOSE=""
 while getopts "v" option "$@"
 do
   case ${option} in
-	v) VERBOSE=1 ;;
+	v) VERBOSE=1; G_VERBOSE=" -v " ;;
   esac
 done
 
@@ -30,15 +31,18 @@ function addslashesforsed
 
 # Try to determine Java homedirectory
 JAVA_HOME="/usr/lib/jvm/java-7-openjdk"
+JAVA_HOME_ALT="/usr/lib/jvm/java-6-openjdk"
 if [ ! -d $JAVA_HOME ]
 then
   JAVA_HOME="/usr/lib/jvm/java-7-openjdk-amd64"
+  JAVA_HOME_ALT="/usr/lib/jvm/java-6-openjdk-amd64"
   if [ ! -d $JAVA_HOME ]
   then
 	JAVA_HOME="/usr/lib/jvm/java-6-openjdk"
+	JAVA_HOME_ALT=""
 	if [ ! -d $JAVA_HOME ]
 	then
-	  JAVA_HOME="/usr/lib/jvm/java-6-openjdk-amd64"
+	  JAVA_HOME=""
 	  if [ ! -d $JAVA_HOME ]
 	  then
 		JAVA_HOME=""
@@ -46,7 +50,17 @@ then
 	fi
   fi
 fi
-
+# If two Java installations are found, let the user pick one
+if [ -d $JAVA_HOME_ALT ]
+then
+  echo -ne "$NM: Two Java installations were found:\n    [0] $JAVA_HOME\n    [1] $JAVA_HOME_ALT\n  Please pick one of these (default is [0]): "
+  read choice
+  if [ " $choice" = " 1" ]
+  then
+	JAVA_HOME=$JAVA_HOME_ALT
+  fi
+fi
+# No Java installation? :(
 if [ -z $JAVA_HOME ]
 then
   echo -e "$NM: [EE] Could not find Java home. Looked at:\n\t'/usr/lib/jvm/java-6-openjdk',\n\t'/usr/lib/jvm/java-6-openjdk-amd64'\n\t'/usr/lib/jvm/java-7-openjdk',\n\t'/usr/lib/jvm/java-7-openjdk-amd64'."
@@ -59,13 +73,18 @@ fi
 # Check library path
 LIST_SEP="\n    - "
 LIST_SEP_ESC=`echo "$LIST_SEP" | addslashesforsed`
-JAVA_LB_PATH=`java getLibraryPath | awk -F: -v ORS="$LIST_SEP" '{for (i = 1; i <= NF; i++) print $i}' | sed '$d'`
+CMD="$JAVA_HOME/jre/bin/java getLibraryPath"
+if [ $VERBOSE -eq 1 ]
+then
+  echo "$NM: [II] Running '$CMD'..."
+fi
+JAVA_LB_PATH=`$CMD | awk -F: -v ORS="$LIST_SEP" '{for (i = 1; i <= NF; i++) print $i}' | sed '$d'`
 
 
 # START OF COMPILE PIECE
 echo "$NM: [II] Now compiling..."
 
-CMD="g++ -I $JAVA_HOME/include -I$JAVA_HOME/include/linux -I/usr/include/qt4 -O0 -g -Wall -Werror -shared -fPIC -lQtGui -lQtCore -o libnativeTextRenderer.so joxy_utils_JoxyGraphics.cpp joxy_utils_JoxyGraphics.h"
+CMD="g++ $G_VERBOSE -I$JAVA_HOME/include -I$JAVA_HOME/include/linux -I/usr/include/qt4 -O0 -g -Wall -Werror -shared -fPIC -lQtGui -lQtCore -o libnativeTextRenderer.so joxy_utils_JoxyGraphics.cpp joxy_utils_JoxyGraphics.h"
 if [ $VERBOSE -eq 1 ]
 then
   echo "$NM: [II] Running '$CMD'..."
--- test/TestGUI.java
+++ test/TestGUI.java
@@ -4,17 +4,11 @@ import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.Rectangle;
 import java.awt.RenderingHints;
-import java.awt.Shape;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-import java.awt.geom.AffineTransform;
-import java.awt.geom.PathIterator;
-import java.awt.geom.Point2D;
-import java.awt.geom.Rectangle2D;
 import java.util.Vector;
 
 import javax.swing.BoxLayout;
