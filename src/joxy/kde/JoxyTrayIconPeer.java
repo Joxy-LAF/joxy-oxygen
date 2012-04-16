3f9b0f912335946ec4304c044f693b171446f54e
Willem Sonke <willem3141@users.sourceforge.net>
2012-04-16 20:44:19 +0200

    Made a first attempt in using a JoxyToolkit for the notification system. Note that this will not compile in Eclipse, unless you disable the error for access restrictions: Eclipse thinks that inheriting from a hidden Java class is not a good idea...

---
+++ src/joxy/kde/JoxyTrayIconPeer.java
@@ -0,0 +1,45 @@
+package joxy.kde;
+
+import java.awt.AWTException;
+import java.awt.TrayIcon;
+
+import java.awt.TrayIcon.MessageType;
+import java.awt.peer.*;
+
+import joxy.utils.Utils;
+
+@SuppressWarnings("restriction")
+public class JoxyTrayIconPeer implements TrayIconPeer {
+
+	private String tooltip = "";
+	
+	public JoxyTrayIconPeer(TrayIcon target) throws AWTException {
+		// Well... probably should do something?
+	}
+
+	@Override
+	public void dispose() {
+		throw new UnsupportedOperationException("Not implemented: dispose");
+	}
+
+	@Override
+	public void setToolTip(String tooltip) {
+		this.tooltip = tooltip;
+	}
+
+	@Override
+	public void updateImage() {
+		throw new UnsupportedOperationException("Not implemented: updateImage");
+	}
+
+	@Override
+	public void displayMessage(String caption, String text, String messageType) {
+		Utils.displayNotification(caption, text, MessageType.INFO);
+	}
+
+	@Override
+	public void showPopupMenu(int x, int y) {
+		throw new UnsupportedOperationException("Not implemented: showPopupMenu");
+	}
+
+}
