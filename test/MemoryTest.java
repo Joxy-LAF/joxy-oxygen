15c71fcb2a908cc4ea20e52f497db4b73eaaf9c5
Willem Sonke <willem3141@users.sourceforge.net>
2012-04-18 16:13:23 +0200

    Created a memory test. The native text rendering cache turns out to consume very much memory. Run this test with the system monitor to see the memory usage of the java process increase.

---
+++ test/MemoryTest.java
@@ -0,0 +1,66 @@
+import java.awt.*;
+import java.awt.TrayIcon.MessageType;
+import java.awt.event.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.Vector;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import javax.swing.table.AbstractTableModel;
+import javax.swing.table.TableModel;
+
+public class MemoryTest {
+	
+	static JFrame frame;
+	
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					UIManager.setLookAndFeel("joxy.JoxyLookAndFeel");
+					//UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
+					//UIManager.setLookAndFeel("com.sun.java.swing.plaf.gtk.GTKLookAndFeel");
+				} catch (ClassNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (InstantiationException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IllegalAccessException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (UnsupportedLookAndFeelException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+				showSomeGUI();
+			}
+		});
+	}
+
+	protected static void showSomeGUI() {
+		frame = new JFrame("Memory test for Joxy");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		final JLabel label = new JLabel("starting");
+		
+		Timer t = new Timer(10, new ActionListener() {
+			
+			long value = 10000000000000l;
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				value++;
+				label.setText("This is a memory test for Joxy. We want to see how much memory the string caching needs. The current value is " + value);
+			}
+		});
+		t.start();
+		
+		frame.add(label);
+		
+		frame.pack();
+		frame.setVisible(true);
+	}
+}
