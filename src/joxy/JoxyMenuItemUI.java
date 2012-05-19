663cb069a7bcb9d961f1c40c8e712e878614dc35
Willem Sonke <willem3141@users.sourceforge.net>
2012-05-19 15:38:46 +0200

    Stop overriding the complete paint method, but only override its sub-methods to get all stuff like accelerators etc.

--- src/joxy/JoxyMenuItemUI.java
+++ src/joxy/JoxyMenuItemUI.java
@@ -156,9 +156,7 @@ public class JoxyMenuItemUI extends BasicMenuItemUI {
 	}
 	
 	@Override
-	public void paint(Graphics g, JComponent c) {
-		
-		JMenuItem mi = (JMenuItem) c;
+	protected void paintBackground(Graphics g, JMenuItem mi, Color bgColor) {
 		
 		Graphics2D g2 = (Graphics2D) g;
 		
@@ -167,82 +165,18 @@ public class JoxyMenuItemUI extends BasicMenuItemUI {
 		
 		// draw the hover
 		MenuItemBackgroundPainter.paint(g2, 2, 1, mi.getWidth() - 4, mi.getHeight() - 3, hoverAmount);
-		
-		g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_LCD_HRGB);
-
-		// Layout the menu item, i.e. determine the place for icon and text
-		FontMetrics f = mi.getFontMetrics(mi.getFont());
-		String clippedText = layout(mi, f, c.getWidth(), c.getHeight());
-		
-		// Draw icon
-		if (mi.getIcon() != null) {
-			mi.getIcon().paintIcon(mi, g2, paintIconR.x, paintIconR.y);
-		}
-		
-		// Draw text
-		g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_LCD_HRGB);
-		g2.setColor(Color.BLACK);
-		g2.setFont(mi.getFont());
-		View v = (View) c.getClientProperty(BasicHTML.propertyKey);
-		if (v != null) { // Text contains HTML
-			v.paint(g2, paintTextR);
-		} else { // No HTML, draw ourselves
-			int w = f.stringWidth(clippedText);
-			int h = f.getHeight();
-			// TODO [ws] That 3 added to the x coordinate is to be consistent with JoxyMenuUI.
-			JoxyGraphics.drawString(g2, clippedText, paintTextR.x + (paintTextR.width - w) / 2 + 3, paintTextR.y + (paintTextR.height + h) / 2 - 3);
-		}
 	}
 	
-	/**
-	 * This method is copied from the BasicLabelUI class.
-	 * What it does exactly (especially in combination with the "layoutCL" method
-	 * that is also copied from BasicLabelUI) we don't quite understand, but hey,
-	 * it works...
-	 */
-	private String layout(JMenuItem mi, FontMetrics fm, int width, int height) {
-		Insets insets = mi.getInsets(null);
-		String text = mi.getText();
-		Icon icon = (mi.isEnabled()) ? mi.getIcon() : mi.getDisabledIcon();
-		Rectangle paintViewR = new Rectangle();
-		paintViewR.x = insets.left;
-		paintViewR.y = insets.top;
-		paintViewR.width = width - (insets.left + insets.right);
-		paintViewR.height = height - (insets.top + insets.bottom);
-		paintIconR.x = paintIconR.y = paintIconR.width = paintIconR.height = 0;
-		paintTextR.x = paintTextR.y = paintTextR.width = paintTextR.height = 0;
-		return layoutCL(mi, fm, text, icon, paintViewR, paintIconR,
-				paintTextR);
+	@Override
+	protected void paintText(Graphics g, JMenuItem menuItem, Rectangle textRect, String text) {
+		Graphics2D g2 = (Graphics2D) g;
+		g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_LCD_HRGB);
+		
+		FontMetrics f = menuItem.getFontMetrics(menuItem.getFont());
+		
+		int w = f.stringWidth(text);
+		int h = f.getHeight();
+		
+		JoxyGraphics.drawString(g2, text, textRect.x, textRect.y + (textRect.height + h) / 2 - 3);
 	}
-	
-    /**
-     * Forwards the call to SwingUtilities.layoutCompoundLabel().
-     * This method is here so that a subclass could do Label specific
-     * layout and to shorten the method name a little.
-     *
-     * @see SwingUtilities#layoutCompoundLabel
-     */
-    protected String layoutCL(
-        JMenuItem mi,
-        FontMetrics fontMetrics,
-        String text,
-        Icon icon,
-        Rectangle viewR,
-        Rectangle iconR,
-        Rectangle textR)
-    {
-        return SwingUtilities.layoutCompoundLabel(
-            mi,
-            fontMetrics,
-            text,
-            icon,
-            mi.getVerticalAlignment(),
-            mi.getHorizontalAlignment(),
-            mi.getVerticalTextPosition(),
-            mi.getHorizontalTextPosition(),
-            viewR,
-            iconR,
-            textR,
-            mi.getIconTextGap());
-    }
 }
