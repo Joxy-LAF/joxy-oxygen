7a1febc0f45ad0dcfb312fa5d8e16b0d73c27826
Willem Sonke <willem3141@users.sourceforge.net>
2012-07-21 15:22:53 +0200

    The arrow button in a JComboBox should not have a hover effect

--- src/joxy/JoxyArrowButton.java
+++ src/joxy/JoxyArrowButton.java
@@ -47,7 +47,11 @@ public class JoxyArrowButton extends BasicArrowButton {
 		// black arrow
 		paintRect = new Rectangle2D.Double(center.getX() - width / 2, center.getY() - height / 2, width, height);
 		if (isEnabled()) {
-			g2.setColor(ColorUtils.mix(Color.BLACK, UIManager.getColor("Button.hover"), ((JoxyButtonUI) getUI()).hoverAmount / 255f));
+			if (getName() != null && getName().equals("ComboBox.arrowButton")) {
+				g2.setColor(Color.BLACK);
+			} else {
+				g2.setColor(ColorUtils.mix(Color.BLACK, UIManager.getColor("Button.hover"), ((JoxyButtonUI) getUI()).hoverAmount / 255f));
+			}
 		} else {
 			g2.setColor(Color.GRAY);
 		}
