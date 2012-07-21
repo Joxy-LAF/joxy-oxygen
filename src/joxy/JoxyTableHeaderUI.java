1c2cf5382d6345bb86b515a9cb77a403aac94c49
Willem Sonke <willem3141@users.sourceforge.net>
2012-07-21 14:34:57 +0200

    Javadoc in JTableHeader

--- src/joxy/JoxyTableHeaderUI.java
+++ src/joxy/JoxyTableHeaderUI.java
@@ -7,6 +7,7 @@ import javax.swing.LookAndFeel;
 import javax.swing.UIManager;
 import javax.swing.plaf.ComponentUI;
 import javax.swing.plaf.basic.BasicTableHeaderUI;
+import javax.swing.table.JTableHeader;
 import javax.swing.table.TableColumn;
 import javax.swing.table.TableColumnModel;
 
@@ -15,7 +16,7 @@ import joxy.utils.JoxyGraphics;
 /**
  * Joxy's UI delegate for the JTableHeader.
  * 
- * <p>There still is a lot of work to do in this class.</p>
+ * <p>The paint methods are overridden to let the table header look more KDE-like.</p>
  * 
  * @author Thom Castermans
  * @author Willem Sonke
@@ -39,13 +40,18 @@ public class JoxyTableHeaderUI extends BasicTableHeaderUI {
 		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 		g2.setRenderingHint(RenderingHints.KEY_ALPHA_INTERPOLATION, RenderingHints.VALUE_ALPHA_INTERPOLATION_QUALITY);
 		
-		// first render the background lines
+		// first render the background
 		renderBackground(g2, c);
 		
 		// then render the names of the columns
 		renderColumns(g2, c);
 	}
-
+	
+	/**
+	 * Paints the background of the table header.
+	 * @param g2 The {@link Graphics2D} object to paint with.
+	 * @param c The {@link JTableHeader} object to paint the background for.
+	 */
 	public void renderBackground(Graphics2D g2, JComponent c) {
 		g2.setColor(UIManager.getColor("Window.background"));
 		g2.fillRect(0, 0, c.getWidth(), c.getHeight());
@@ -56,6 +62,12 @@ public class JoxyTableHeaderUI extends BasicTableHeaderUI {
 		g2.drawLine(0, c.getHeight() - 1, c.getWidth(), c.getHeight() - 1);
 	}
 	
+	/**
+	 * Paints the column names and the separators between them. This method is partially
+	 * copied from {@link BasicTableHeaderUI#paint(Graphics, JComponent)}.
+	 * @param g2 The {@link Graphics2D} object to paint with.
+	 * @param c The {@link JTableHeader} object to paint the columns for.
+	 */
 	public void renderColumns(Graphics2D g2, JComponent c) {
 		if (header.getColumnModel().getColumnCount() <= 0) {
             return;
@@ -69,13 +81,13 @@ public class JoxyTableHeaderUI extends BasicTableHeaderUI {
 		int cMin = header.columnAtPoint(left);
 		int cMax = header.columnAtPoint(right);
 
-		// This should never happen.
+		// this should never happen
 		if (cMin == -1) {
 			cMin = 0;
 		}
 
-		// If the table does not have enough columns to fill the view we'll get -1.
-		// Replace this with the index of the last column.
+		// if the table does not have enough columns to fill the view we'll get -1;
+		// replace this with the index of the last column
 		if (cMax == -1) {
 			cMax = cm.getColumnCount() - 1;
 		}
@@ -85,6 +97,7 @@ public class JoxyTableHeaderUI extends BasicTableHeaderUI {
 		Rectangle cellRect = header.getHeaderRect(cMin);
 		TableColumn aColumn;
         
+		// loop over all columns and paint them
 		for (int column = cMin; column <= cMax; column++) {
 			aColumn = cm.getColumn(column);
 			columnWidth = aColumn.getWidth();
@@ -92,7 +105,7 @@ public class JoxyTableHeaderUI extends BasicTableHeaderUI {
 			if (aColumn != draggedColumn) {
 				paintColumnName(g2, cellRect, column);
 				if (column != cMin) {
-					paintColumnSeparator(g2, cellRect, column);
+					paintColumnSeparator(g2, cellRect);
 				}
 			}
 			cellRect.x += columnWidth;
@@ -101,6 +114,13 @@ public class JoxyTableHeaderUI extends BasicTableHeaderUI {
 		// TODO render the dragged column
 	}
 	
+	/**
+	 * Paints the name of a column.
+	 * @param g2 The {@link Graphics2D} object to paint with.
+	 * @param cellRect A {@link Rectangle} object that indicates the size and position of
+	 * the column.
+	 * @param columnIndex The index of the column (used for finding the name).
+	 */
 	private void paintColumnName(Graphics2D g2, Rectangle cellRect, int columnIndex) {
 		g2.setColor(Color.BLACK);
 		String name = header.getColumnModel().getColumn(columnIndex).getHeaderValue().toString();
@@ -112,8 +132,11 @@ public class JoxyTableHeaderUI extends BasicTableHeaderUI {
 	
 	/**
 	 * Paints a column separator <i>before</i> the given column.
+	 * @param g2 The {@link Graphics2D} object to paint with.
+	 * @param cellRect A {@link Rectangle} object that indicates the size and position of
+	 * the column.
 	 */
-	private void paintColumnSeparator(Graphics2D g2, Rectangle cellRect, int columnIndex) {
+	private static void paintColumnSeparator(Graphics2D g2, Rectangle cellRect) {
 		g2.setColor(new Color(220, 220, 220));
 		g2.drawOval(cellRect.x, cellRect.y + cellRect.height / 2 - 3, 1, 1);
 		g2.drawOval(cellRect.x, cellRect.y + cellRect.height / 2, 1, 1);
