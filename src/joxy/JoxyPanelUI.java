811dacffb335d59cb1bbd834c9d2a2bfabba1274
Willem Sonke <willem3141@users.sourceforge.net>
2012-08-22 17:35:40 +0200

    Replace Java default borders with their KDE equivalents
    This is implemented by looking at the border type in JPanel, and then just setting the border to a different type. Some notes:
    
    * Compound borders are not being swapped, because the programmer probably has done his very best to create a beautiful border, and we don't want to spoil that :-)
    * Line borders are not being swapped because there is no KDE equivalent
    * Etched borders are being swapped to KDE "bevel borders"
    * For some reason borders are not swapped back when Joxy is removed, although that should happen.
    * Titled borders are to be implemented, but I think there is some special mechanism for these, that we haven't discovered yet.
    * This obviously doesn't work on other JComponents than the JPanel, but actually Swing documentation advises programmers not to change the borders on other JComponents. So I guess we can ignore it.

--- src/joxy/JoxyPanelUI.java
+++ src/joxy/JoxyPanelUI.java
@@ -3,8 +3,13 @@ package joxy;
 import java.awt.Graphics;
 
 import javax.swing.JComponent;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.Border;
+import javax.swing.border.EtchedBorder;
 import javax.swing.plaf.ComponentUI;
 import javax.swing.plaf.basic.BasicPanelUI;
+
+import joxy.border.JoxyBevelBorder;
 
 /**
  * Joxy's UI delegate for the JPanel.
@@ -16,16 +21,50 @@ import javax.swing.plaf.basic.BasicPanelUI;
  */
 public class JoxyPanelUI extends BasicPanelUI {
 	
+	Border oldBorder;
+	
 	public static ComponentUI createUI(JComponent c) {
-		c.setOpaque(false);
 		JoxyPanelUI panelUI = new JoxyPanelUI();
 		return panelUI;
 	}
 	
 	@Override
-	public void paint(Graphics g, JComponent c) {
-		// TODO Auto-generated method stub
-		super.paint(g, c);
+	public void installUI(JComponent c) {
+		super.installUI(c);
+		
+		c.setOpaque(false);
+	}
+	
+	@Override
+	public void uninstallUI(JComponent c) {
+		super.uninstallUI(c);
+		
+		// revert the Joxy border to the default border
+		// TODO for some reason, this doesn't work yet...
+		if (oldBorder != null) {
+			c.setBorder(oldBorder);
+		}
+	}
+	
+	@Override
+	public void update(Graphics g, JComponent c) {
+		
+		// in this method, we try to swap the default Swing borders for nice KDE-like borders
+		Border b = c.getBorder();
+		
+		// note that setBorder repaints the component itself; if we also would do that we
+		// get a StackOverflowError
+		if (b instanceof BevelBorder) {
+			oldBorder = b;
+			c.setBorder(new JoxyBevelBorder(((BevelBorder) b).getBevelType()));
+		}
+		if (b instanceof EtchedBorder) {
+			oldBorder = b;
+			// TODO this is not the correct Joxy border type, but we don't have a real etched border in KDE
+			c.setBorder(new JoxyBevelBorder(((EtchedBorder) b).getEtchType()));
+		}
+		
+		super.update(g, c);
 	}
 
 }
---
+++ src/joxy/border/JoxyBevelBorder.java
@@ -0,0 +1,74 @@
+package joxy.border;
+
+import java.awt.*;
+
+import javax.swing.border.BevelBorder;
+
+import joxy.JoxyPanelUI;
+
+/**
+ * This class implements a more KDE-like variant of the {@link BevelBorder}.
+ * 
+ * <p>We try to substitute Joxy-specific borders for the default Java Swing
+ * borders in the {@link JoxyPanelUI} class.</p>
+ * 
+ * @author Thom Castermans
+ * @author Willem Sonke
+ */
+public class JoxyBevelBorder extends BevelBorder {
+
+	private static final int ARC = 6;
+	
+	/**
+	 * Creates a new Joxy bevel border of the specified type. Contrary to the
+	 * {@link BevelBorder}, it is not possible to set the colours manually.
+	 * @param type The type of the border, either BevelBorder.LOWERED
+	 * (produces a "lowered" border) or BevelBorder.RAISED (a "raised" border).
+	 */
+	public JoxyBevelBorder(int type) {
+		super(type);
+    }
+	
+	@Override
+	public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
+
+		Graphics2D g2 = (Graphics2D) g;
+		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		g2.setRenderingHint(RenderingHints.KEY_ALPHA_INTERPOLATION, RenderingHints.VALUE_ALPHA_INTERPOLATION_QUALITY);
+		
+		if (bevelType == BevelBorder.RAISED) {
+			
+			// shadow
+			g2.setStroke(new BasicStroke(0.5f));
+			GradientPaint shadowGradient = new GradientPaint(0, 0, new Color(0, 0, 0, 0), 0, 3, new Color(0, 0, 0, 80));
+			g2.setPaint(shadowGradient);
+			g2.drawRoundRect(x, y, width - 1, height - 2, ARC + 2, ARC + 2);
+			g2.setColor(new Color(0, 0, 0, 40));
+			g2.drawRoundRect(x, y, width - 1, height - 1, ARC + 2, ARC + 2);
+			
+			// white border
+			g2.setColor(Color.WHITE);
+			g2.drawRoundRect(x + 1, y + 1, width - 3, height - 4, ARC, ARC);
+			
+		} else {
+
+			// outer border
+			g2.setStroke(new BasicStroke(0.5f));
+			LinearGradientPaint outerGradient = new LinearGradientPaint(0, y + 1, 0, height - 4,
+					new float[]{0, 3f / height, 1 - 2f / height, 1},
+					new Color[]{new Color(0, 0, 0, 180), new Color(0, 0, 0, 90),
+					             new Color(0, 0, 0, 90), new Color(0, 0, 0, 30)});
+			g2.setPaint(outerGradient);
+			g2.drawRoundRect(x + 1, y + 1, width - 3, height - 4, ARC, ARC);
+			
+			// inner border
+			LinearGradientPaint innerGradient = new LinearGradientPaint(0, y + 2, 0, height - 6,
+					new float[]{0, 3f / height, 1 - 2f / height, 1},
+					new Color[]{new Color(0, 0, 0, 90), new Color(0, 0, 0, 15),
+					             new Color(0, 0, 0, 15), new Color(0, 0, 0, 0)});
+			g2.setPaint(innerGradient);
+			g2.drawRoundRect(x + 2, y + 2, width - 5, height - 6, ARC - 2, ARC - 2);
+			
+		}
+	}
+}
