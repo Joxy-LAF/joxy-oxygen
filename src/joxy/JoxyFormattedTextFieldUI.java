a62bdbfbcf3d2e93a48bd179f8c8bd1518820182
Willem Sonke <willem3141@users.sourceforge.net>
2012-08-23 11:51:52 +0200

    Porting JTextField changes to JFormattedTextField

--- src/joxy/JoxyFormattedTextFieldUI.java
+++ src/joxy/JoxyFormattedTextFieldUI.java
@@ -1,28 +1,79 @@
 package joxy;
 
 import java.awt.*;
+import java.awt.event.*;
 
 import javax.swing.*;
 import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 import javax.swing.plaf.ComponentUI;
 import javax.swing.plaf.basic.BasicFormattedTextFieldUI;
 import javax.swing.plaf.basic.BasicTextFieldUI;
 import javax.swing.text.*;
 
 import joxy.painter.InputFieldPainter;
+import joxy.painter.TextFieldFocusIndicatorPainter;
+import joxy.painter.TextFieldHoverIndicatorPainter;
+import joxy.utils.Utils;
 
+/**
+ * Joxy's UI delegate for the JFormattedTextField.
+ * 
+ * <p>The JoxyFormattedTextFieldUI supports animations for the focus and hovered states.
+ * See JoxyButtonUI for more details. Furthermore the clear button in the
+ * text field is animated.</p>
+ * 
+ * @author Thom Castermans
+ * @author Willem Sonke
+ */
 public class JoxyFormattedTextFieldUI extends BasicFormattedTextFieldUI {
-
-	/**
-	 * The width and height of the arcs that form up
-	 * the corners of the rounded rectangles. 
-	 */
-	public static final int ARC = 8;
 	
 	/**
 	 * The text field we are painting for.
 	 */
 	JFormattedTextField textField;
+	
+	/**
+	 * A {@link MouseListener} that clears the contents of the text field when
+	 * the clear button has been pressed.
+	 */
+	MouseListener clearListener;
+	
+	/**
+	 * A {@link DocumentListener} that hides the clear button if there is no
+	 * text in the text field.
+	 */
+	DocumentListener changeListener;
+	
+	/** Timer for the clear button animation */
+	private Timer clearButtonTimer;
+	
+	/**
+	 * The opacity of the clear button. 0 means non-visible, 255 is fully opaque.
+	 */
+	int clearButtonOpacity = 0;
+	
+	/**
+	 * Amount of hover and focus, from 0 to 255.
+	 */
+	private int hoverAmount = 0, focusAmount = 0;
+
+	/** Timers for the animation */
+	private Timer hoverTimer, focusTimer;
+	
+	/** Listeners for the animation */
+	private MouseListener hoverListener;
+	private FocusListener focusListener;
+	
+	private boolean hovered = false;
+	
+	/**
+	 * For some reason it doesn't work to add the changeListener to the Document of
+	 * the text field in installListeners(). Therefore we do it in the paint method.
+	 * Of course it only needs to be added once, and therefore we use this variable.
+	 * TODO figure out why just using installListeners() doesn't work
+	 */
+	boolean changeListenerAdded = false;
 	
 	public static ComponentUI createUI(JComponent c) {
 		c.setOpaque(false);
@@ -39,14 +90,188 @@ public class JoxyFormattedTextFieldUI extends BasicFormattedTextFieldUI {
 		super.installDefaults();
 		
 		// [ws] TODO deze dingen kunnen eigenlijk ook gewoon in de defaults...
-		textField.setBorder(BorderFactory.createEmptyBorder(3, 5, 3, 5));
+		// [ws] TODO This is bad, since the clear button could not be shown, and then the 25
+		// pixels on the right side are strange
+		textField.setBorder(BorderFactory.createEmptyBorder(3, 5, 5, 25));
 		textField.setFont(UIManager.getFont("Button.font"));
 		//textField.setSelectedTextColor(UIManager.getColor("TextField.selectionBackground"));
 	}
 	
 	@Override
+	protected void installListeners() {
+		super.installListeners();
+		
+		clearListener = new MouseListener() {
+			
+			@Override
+			public void mouseReleased(MouseEvent e) {}
+			
+			@Override
+			public void mousePressed(MouseEvent e) {}
+			
+			@Override
+			public void mouseExited(MouseEvent e) {}
+			
+			@Override
+			public void mouseEntered(MouseEvent e) {}
+			
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				
+				// Clear the text if
+				// 1. the clear button is visible;
+				// 2. the click happened inside the clear button;
+				// 3. the text field is not an editor, for example in an editable JComboBox,
+				//    that manages its own borders et cetera;
+				// 4. the text field is editable (in fact, the clear button should not be
+				//    visible if the field is non-editable, but for some reason that still
+				//    happens).
+				if (clearButtonOpacity > 0 && e.getX() > textField.getWidth() - 24
+						          && textField.getClientProperty("joxy.isEditor") == null
+						          && textField.isEditable()) {
+					textField.setText("");
+				}
+			}
+		};
+		
+		textField.addMouseListener(clearListener);
+		
+		changeListener = new DocumentListener() {
+
+			@Override
+			public void insertUpdate(DocumentEvent e) {
+				updateClearButton();
+			}
+
+			@Override
+			public void removeUpdate(DocumentEvent e) {
+				updateClearButton();
+			}
+
+			@Override
+			public void changedUpdate(DocumentEvent e) {
+				updateClearButton();
+			}
+			
+			private void updateClearButton() {
+				clearButtonTimer.start();
+			}
+		};
+		
+		hoverListener = new MouseListener() {
+			
+			@Override
+			public void mouseReleased(MouseEvent e) {}
+			
+			@Override
+			public void mousePressed(MouseEvent e) {}
+			
+			@Override
+			public void mouseClicked(MouseEvent e) {}
+			
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				hovered = true;
+				hoverTimer.start();
+			}
+			
+			@Override
+			public void mouseExited(MouseEvent e) {
+				hovered = false;
+				hoverTimer.start();
+			}
+		};
+		textField.addMouseListener(hoverListener);
+		
+		focusListener = new FocusListener() {
+			
+			@Override
+			public void focusLost(FocusEvent e) {
+				focusTimer.start();
+			}
+			
+			@Override
+			public void focusGained(FocusEvent e) {
+				focusTimer.start();
+			}
+		};
+		textField.addFocusListener(focusListener);
+			
+		createTimers();
+	}
+	
+	private void createTimers() {
+		clearButtonTimer = new Timer(40, new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (textField.getText().length() > 0) {
+					clearButtonOpacity += 70;
+				} else {
+					clearButtonOpacity -= 70;
+				}
+				if (clearButtonOpacity > 255) {
+					clearButtonOpacity = 255;
+					clearButtonTimer.stop();
+				}
+				if (clearButtonOpacity < 0) {
+					clearButtonOpacity = 0;
+					clearButtonTimer.stop();
+				}
+				textField.repaint();
+			}
+		});
+		
+		// start the timer once, to ensure the button is visible if there is text
+		// in the text field initially
+		clearButtonTimer.start();
+		
+		hoverTimer = new Timer(40, new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (hovered) {
+					hoverAmount += 60;
+				} else {
+					hoverAmount -= 60;
+				}
+				if (hoverAmount > 255) {
+					hoverAmount = 255;
+					hoverTimer.stop();
+				}
+				if (hoverAmount < 0) {
+					hoverAmount = 0;
+					hoverTimer.stop();
+				}
+				textField.repaint();
+			}
+		});
+		
+		focusTimer = new Timer(40, new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (textField.hasFocus()) {
+					focusAmount += 60;
+				} else {
+					focusAmount -= 60;
+				}
+				if (focusAmount > 255) {
+					focusAmount = 255;
+					focusTimer.stop();
+				}
+				if (focusAmount < 0) {
+					focusAmount = 0;
+					focusTimer.stop();
+				}
+				textField.repaint();
+			}
+		});
+	}
+	
+	@Override
 	protected String getPropertyPrefix() {
-		return "TextField";
+		return "FormattedTextField";
 	}
 	
 
@@ -348,7 +573,25 @@ public class JoxyFormattedTextFieldUI extends BasicFormattedTextFieldUI {
 	 */
 	@Override
 	protected void paintSafely(Graphics g) {
-		paintBackground(g);
+		
+		// see comment of changeListenerAdded
+		if (!changeListenerAdded) {
+			changeListenerAdded = true;
+			textField.getDocument().addDocumentListener(changeListener);
+		}
+		
+		Graphics2D g2 = (Graphics2D) g;
+		
+		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		g2.setRenderingHint(RenderingHints.KEY_ALPHA_INTERPOLATION, RenderingHints.VALUE_ALPHA_INTERPOLATION_QUALITY);
+		
+		if (textField.isEditable() && textField.getClientProperty("joxy.isEditor") == null) {
+			paintBackground(g);
+			
+			if (clearButtonOpacity > 0) {
+				paintClearButton(g, clearButtonOpacity);
+			}
+		}
 		
 		Highlighter highlighter = textField.getHighlighter();
         Caret caret = textField.getCaret();
@@ -368,19 +611,33 @@ public class JoxyFormattedTextFieldUI extends BasicFormattedTextFieldUI {
         if (caret != null) {
             caret.paint(g);
         }
+        
+        ((Graphics2D) g).setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_LCD_HRGB);
 	}
-    
-    @Override
+	
+	@Override
     protected void paintBackground(Graphics g) {
 		Graphics2D g2 = (Graphics2D) g;
 		
-		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-		g2.setRenderingHint(RenderingHints.KEY_ALPHA_INTERPOLATION, RenderingHints.VALUE_ALPHA_INTERPOLATION_QUALITY);
-
-		if (textField.getClientProperty("joxy.isEditor") == null) {
-		    InputFieldPainter.paint(g2, 1, 1, textField.getWidth() - 2, textField.getHeight() - 2);
-		}
-
-		g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_LCD_HRGB);
+		InputFieldPainter.paint(g2, 0, 0, textField.getWidth(), textField.getHeight());
+		TextFieldFocusIndicatorPainter.paint(g2, 0, 0, textField.getWidth(), textField.getHeight(), focusAmount);
+		TextFieldHoverIndicatorPainter.paint(g2, 0, 0, textField.getWidth(), textField.getHeight(), Math.max(0, hoverAmount - focusAmount));
     }
+    
+    private void paintClearButton(Graphics g, int opacity) {
+		Graphics2D g2 = (Graphics2D) g;
+		
+		// TODO this icon should be cached
+		ImageIcon clearIcon = Utils.getOxygenIcon("actions/edit-clear-locationbar-rtl", 16);
+		
+		if (clearIcon == null) {
+			return;
+		}
+		
+		clearIcon.paintIcon(textField, g2, textField.getWidth() - 20, textField.getHeight() / 2 - 8);
+		
+		// TODO this is ugly
+		g2.setColor(new Color(255, 255, 255, 255 - opacity));
+		g2.fillRect(textField.getWidth() - 20, textField.getHeight() / 2 - 8, 16, 16);
+	}
 }
