ca41f69b43f2dfb35e90dabbab03cc261ed410f7
Willem Sonke <willem3141@users.sourceforge.net>
2012-10-07 17:36:43 +0200

    Bug fix: UTF-8 text was rendered incorrectly with native text rendering (patch by Vitaliy, thanks!)

--- src/joxy_utils_JoxyGraphics.cpp
+++ src/joxy_utils_JoxyGraphics.cpp
@@ -7,6 +7,7 @@
 #include <QtGui/qcolor.h>
 #include <QtGui/qimage.h>
 #include <QtGui/qpainter.h>
+#include <QtCore/qstring.h>
 
 int argc = 0;
 char** argv = NULL;
@@ -15,51 +16,44 @@ jmethodID mid;
 
 JNIEXPORT void JNICALL Java_joxy_utils_JoxyGraphics_initializeNative
   (JNIEnv *env, jclass cl) {
-
-	jclass cls = env->FindClass("java/awt/image/BufferedImage");
-
-	if (cls == NULL) {
-    	printf("Couldn't find class BufferedImage\n");
+    jclass cls = env->FindClass("java/awt/image/BufferedImage");
+    if (cls == NULL) {
+        printf("Couldn't find class BufferedImage\n");
         return;
     }
-
-	mid = env->GetMethodID(cls, "setRGB", "(III)V");
-
+    mid = env->GetMethodID(cls, "setRGB", "(III)V");
     if (mid == NULL) {
-    	printf("Couldn't find method setRGB(int)\n");
+        printf("Couldn't find method setRGB(int)\n");
         return;
     }
 }
 
 JNIEXPORT void JNICALL Java_joxy_utils_JoxyGraphics_drawStringNative
-  (JNIEnv *env, jclass cl, jstring str, jobject image, jint width, jint height, jstring fontname, jint fontsize, jint style, jint color) {
-
+  (JNIEnv *env, jclass cl, jstring str, jobject image, jint width,
+jint height, jstring fontname, jint fontsize, jint style, jint color)
+{
     QImage qimage(width, height, QImage::Format_ARGB32);
     QColor qcolor = QColor::fromRgb(color);
     qcolor.setAlpha(0);
     qimage.fill(qcolor);
     QPainter painter(&qimage);
     painter.setPen(QColor::fromRgb(color));
-
     const char* cfontname = env->GetStringUTFChars(fontname, JNI_FALSE);
     bool italic = (style >= 2);
     bool bold = (style % 2 == 1);
     painter.setFont(QFont(cfontname, fontsize, (bold ? 75 : 50), italic));
     const char* cstr = env->GetStringUTFChars(str, JNI_FALSE);
-    painter.drawText(0, 0, width + 10, height, Qt::AlignLeft, cstr);
+    painter.drawText(0, 0, width + 10, height, Qt::AlignLeft, QString::fromUtf8(cstr));
     env->ReleaseStringUTFChars(str, cfontname);
     env->ReleaseStringUTFChars(str, cstr);
-
     // TODO do this with the int[] version, so the for-loop is unnecessary
-
-	for (int i = 0; i < width; i++) {
-	    for (int j = 0; j < height; j++) {
-	    	int pixel = qimage.pixel(i, j);
-
-	    	// if it is transparent, it is useless to copy it...
-	    	if (pixel != 0) {
-	    		env->CallVoidMethod(image, mid, i, j, pixel);
-	    	}
-	    }
-	}
+    for (int i = 0; i < width; i++) {
+        for (int j = 0; j < height; j++) {
+            int pixel = qimage.pixel(i, j);
+            // if it is transparent, it is useless to copy it...
+            if (pixel != 0) {
+                env->CallVoidMethod(image, mid, i, j, pixel);
+            }
+        }
+    }
 }
