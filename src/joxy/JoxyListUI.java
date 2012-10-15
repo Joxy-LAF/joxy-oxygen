9002c140a71bb1c3f399b359915556ed02d80f10
Willem Sonke <willem3141@users.sourceforge.net>
2012-10-15 21:17:32 +0200

    Hover and focus animation for JLists
    This is something different than the hover decoration for individual elements. Also fixed a bug that caused the hover decoration to be incorrect for multicolumn JLists, so file choosers should work correctly now. There is only one remaining problem: the hover and focus animations, and the JList's borders, scroll with the JList contents if it is inside a JScrollPane. Some advanced hacks would probably be needed there.

--- src/joxy/JoxyListUI.java
+++ src/joxy/JoxyListUI.java
@@ -1,9 +1,7 @@
 package joxy;
 
 import java.awt.*;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.awt.event.MouseMotionListener;
+import java.awt.event.*;
 import java.awt.geom.RoundRectangle2D;
 
 import javax.swing.*;
@@ -11,17 +9,52 @@ import javax.swing.plaf.ComponentUI;
 import javax.swing.plaf.basic.BasicListUI;
 
 import joxy.painter.InputFieldPainter;
+import joxy.painter.TextFieldFocusIndicatorPainter;
+import joxy.painter.TextFieldHoverIndicatorPainter;
 import joxy.renderer.JoxyListCellRenderer;
+import sun.swing.SwingUtilities2;
 
 /**
- * Only a stub.
+ * Joxy's UI delegate for the JList.
+ * 
+ * <p>The JoxyListUI supports animations for the focus and hovered states.
+ * See JoxyButtonUI for more details.</p>
+ * 
+ * <p>JoxyListUI also handles painting the hover and selected states of
+ * the list elements. To be able to know which element is hovered, an additional
+ * {@link MouseMotionListener} is added to the JList.</p>
  */
 public class JoxyListUI extends BasicListUI {
 
-	// -1: no row is hovered
+	/**
+	 * Index of the currently hovered list element.
+	 */
 	private int hoveredRow = -1;
+	
+	/**
+	 * Whether the JList is hovered at the moment.
+	 */
+	private boolean hovered = false;
+	
+	/**
+	 * Amount of hover and focus, from 0 to 255.
+	 */
+	private int hoverAmount = 0, focusAmount = 0;
 
+	/**
+	 * Timers for the animation.
+	 */
+	private Timer hoverTimer, focusTimer;
+
+	/**
+	 * Listeners for the animation.
+	 */
 	private MouseListener mouseListener;
+	private FocusListener focusListener;
+	
+	/**
+	 * A listener for managing the currently hovered row.
+	 */
 	private MouseMotionListener mouseMotionListener;
 	
 	public static ComponentUI createUI(JComponent c) {
@@ -46,14 +79,20 @@ public class JoxyListUI extends BasicListUI {
 
 			@Override
 			public void mouseEntered(MouseEvent e) {
-				hoveredRow = JoxyListUI.this.convertYToRow(e.getY());
+				hoveredRow = SwingUtilities2.loc2IndexFileList(list, e.getPoint());
 				list.repaint();
+				
+				hovered = true;
+				hoverTimer.start();
 			}
 
 			@Override
 			public void mouseExited(MouseEvent e) {
 				hoveredRow = -1;
 				list.repaint();
+
+				hovered = false;
+				hoverTimer.start();
 			}
 		};
 		
@@ -61,16 +100,86 @@ public class JoxyListUI extends BasicListUI {
 			
 			@Override
 			public void mouseMoved(MouseEvent e) {
-				hoveredRow = JoxyListUI.this.convertYToRow(e.getY()); // TODO this is buggy!
+				hoveredRow = SwingUtilities2.loc2IndexFileList(list, e.getPoint());
 				list.repaint();
 			}
 			
 			@Override
 			public void mouseDragged(MouseEvent e) {}
 		};
-
+		
+		focusListener = new FocusListener() {
+			
+			@Override
+			public void focusLost(FocusEvent e) {
+				focusTimer.start();
+			}
+			
+			@Override
+			public void focusGained(FocusEvent e) {
+				focusTimer.start();
+			}
+		};
+		
 		list.addMouseListener(mouseListener);
 		list.addMouseMotionListener(mouseMotionListener);
+		list.addFocusListener(focusListener);
+		
+		createTimers();
+	}
+	
+	private void createTimers() {
+		
+		hoverTimer = new Timer(40, new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (hovered) {
+					hoverAmount += 60;
+				} else {
+					hoverAmount -= 60;
+				}
+				if (hoverAmount > 255) {
+					hoverAmount = 255;
+					hoverTimer.stop();
+				}
+				if (hoverAmount < 0) {
+					hoverAmount = 0;
+					hoverTimer.stop();
+				}
+				list.repaint();
+			}
+		});
+		
+		focusTimer = new Timer(40, new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (list.hasFocus()) {
+					focusAmount += 60;
+				} else {
+					focusAmount -= 60;
+				}
+				if (focusAmount > 255) {
+					focusAmount = 255;
+					focusTimer.stop();
+				}
+				if (focusAmount < 0) {
+					focusAmount = 0;
+					focusTimer.stop();
+				}
+				list.repaint();
+			}
+		});
+	}
+	
+	@Override
+	protected void uninstallListeners() {
+		super.uninstallListeners();
+		
+		list.removeMouseListener(mouseListener);
+		list.removeMouseMotionListener(mouseMotionListener);
+		list.removeFocusListener(focusListener);
 	}
 	
 	@Override
@@ -89,24 +198,18 @@ public class JoxyListUI extends BasicListUI {
 		
 		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 		g2.setRenderingHint(RenderingHints.KEY_ALPHA_INTERPOLATION, RenderingHints.VALUE_ALPHA_INTERPOLATION_QUALITY);
-		
-		paintBackground(g2, c);
+
+		InputFieldPainter.paint(g2, 0, 0, c.getWidth(), c.getHeight());
 		
 		super.paint(g2, c); // this also paints the contents of the list
 		
-		// TODO only paint hover/focus indicator and shadow here! So split this.
-		// TODO the background scrolls with the content if the JList is in a JScrollPane
-	}
-	
-    private void paintBackground(Graphics2D g2, JComponent c) {
-		
-		InputFieldPainter.paint(g2, 0, 0, c.getWidth(), c.getHeight());
-		
 		if (c.isEnabled()) {
-			//TextFieldFocusIndicatorPainter.paint(g2, 0, 0, c.getWidth(), c.getHeight(), focusAmount);
-			//TextFieldHoverIndicatorPainter.paint(g2, 0, 0, c.getWidth(), c.getHeight(), Math.max(0, hoverAmount - focusAmount));
+			TextFieldFocusIndicatorPainter.paint(g2, 0, 0, c.getWidth(), c.getHeight(), focusAmount);
+			TextFieldHoverIndicatorPainter.paint(g2, 0, 0, c.getWidth(), c.getHeight(), Math.max(0, hoverAmount - focusAmount));
 		}
-    }
+		
+		// TODO the background and focus indicator scroll with the content if the JList is in a JScrollPane
+	}
     
     @Override
     protected void paintCell(Graphics g, int row, Rectangle rowBounds,
