82078e9d8da0160c0131db2da7b86fbb8599cdcf
Willem Sonke <willem3141@users.sourceforge.net>
2012-10-16 20:13:38 +0200

    Fix InvalidArgumentException for the gradients of the border, output a debug message instead

--- src/joxy/border/JoxyTitledBorder.java
+++ src/joxy/border/JoxyTitledBorder.java
@@ -7,6 +7,7 @@ import javax.swing.border.TitledBorder;
 
 import joxy.JoxyPanelUI;
 import joxy.utils.JoxyGraphics;
+import joxy.utils.Output;
 
 /**
  * This class implements a more KDE-like variant of the {@link TitledBorder}.
@@ -37,32 +38,36 @@ public class JoxyTitledBorder extends TitledBorder {
 		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 		g2.setRenderingHint(RenderingHints.KEY_ALPHA_INTERPOLATION, RenderingHints.VALUE_ALPHA_INTERPOLATION_QUALITY);
 		
-		// shadow
-		g2.setStroke(new BasicStroke(0.5f));
-		LinearGradientPaint shadowGradient = new LinearGradientPaint(0, y, 0, height - 2,
-				new float[]{0, 3f / height, 1 - 40f / height, 1},
-				new Color[]{new Color(0, 0, 0, 0), new Color(0, 0, 0, 80), new Color(0, 0, 0, 80), new Color(0, 0, 0, 0)});
-		g2.setPaint(shadowGradient);
-		g2.drawRoundRect(x, y, width - 1, height - 2, ARC + 2, ARC + 2);
-		LinearGradientPaint secondShadowGradient = new LinearGradientPaint(0, y, 0, height - 2,
-				new float[]{1 - 40f / height, 1},
-				new Color[]{new Color(0, 0, 0, 40), new Color(0, 0, 0, 0)});
-		g2.setPaint(secondShadowGradient);
-		g2.drawRoundRect(x, y, width - 1, height - 1, ARC + 2, ARC + 2);
-		
-		// white border
-		LinearGradientPaint borderGradient = new LinearGradientPaint(0, y + 1, 0, height - 4,
-				new float[]{1 - 38f / height, 1},
-				new Color[]{new Color(255, 255, 255, 255), new Color(255, 255, 255, 0)});
-		g2.setPaint(borderGradient);
-		g2.drawRoundRect(x + 1, y + 1, width - 3, height - 4, ARC, ARC);
-		
-		// fill
-		LinearGradientPaint fillGradient = new LinearGradientPaint(0, y + 1, 0, height - 4,
-				new float[]{1 - 38f / height, 1},
-				new Color[]{new Color(255, 255, 255, 30), new Color(255, 255, 255, 0)});
-		g2.setPaint(fillGradient);
-		g2.fillRoundRect(x + 1, y + 1, width - 3, height - 4, ARC, ARC);
+		try {
+			// shadow
+			g2.setStroke(new BasicStroke(0.5f));
+			LinearGradientPaint shadowGradient = new LinearGradientPaint(0, y, 0, height - 2,
+					new float[]{0, 3f / height, 1 - 40f / height, 1},
+					new Color[]{new Color(0, 0, 0, 0), new Color(0, 0, 0, 80), new Color(0, 0, 0, 80), new Color(0, 0, 0, 0)});
+			g2.setPaint(shadowGradient);
+			g2.drawRoundRect(x, y, width - 1, height - 2, ARC + 2, ARC + 2);
+			LinearGradientPaint secondShadowGradient = new LinearGradientPaint(0, y, 0, height - 2,
+					new float[]{1 - 40f / height, 1},
+					new Color[]{new Color(0, 0, 0, 40), new Color(0, 0, 0, 0)});
+			g2.setPaint(secondShadowGradient);
+			g2.drawRoundRect(x, y, width - 1, height - 1, ARC + 2, ARC + 2);
+			
+			// white border
+			LinearGradientPaint borderGradient = new LinearGradientPaint(0, y + 1, 0, height - 4,
+					new float[]{1 - 38f / height, 1},
+					new Color[]{new Color(255, 255, 255, 255), new Color(255, 255, 255, 0)});
+			g2.setPaint(borderGradient);
+			g2.drawRoundRect(x + 1, y + 1, width - 3, height - 4, ARC, ARC);
+			
+			// fill
+			LinearGradientPaint fillGradient = new LinearGradientPaint(0, y + 1, 0, height - 4,
+					new float[]{1 - 38f / height, 1},
+					new Color[]{new Color(255, 255, 255, 30), new Color(255, 255, 255, 0)});
+			g2.setPaint(fillGradient);
+			g2.fillRoundRect(x + 1, y + 1, width - 3, height - 4, ARC, ARC);
+		} catch (IllegalArgumentException e) {
+			Output.debug("IllegalArgumentException from the titled border gradients: " + e.getMessage());
+		}
 		
 		// text
 		g2.setColor(UIManager.getColor("TitledBorder.foreground"));
