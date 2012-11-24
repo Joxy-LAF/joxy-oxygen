ab224caf4860308ebc2d89e3a984a3b003f4df5b
Willem Sonke <willem3141@users.sourceforge.net>
2012-11-24 11:01:17 +0100

    Introduce a new USE_CACHING option, such that caching can be switched on or off
    A mechanism still needs to be implemented to switch options like this on or off at runtime, like the following:
    
    java -Djoxy.usepaintercaching=false -Djoxy.taboverflow=true joxy.test.TestGUI
    
    This way, users can put the options in configuration files for them to apply.

--- src/joxy/painter/Painter.java
+++ src/joxy/painter/Painter.java
@@ -22,6 +22,11 @@ public abstract class Painter {
 	private BufferedImage cache = null;
 	
 	/**
+	 * Whether to use caching or not.
+	 */
+	public static final boolean USE_CACHING = true;
+	
+	/**
 	 * Paints the object given the currently set data, using the {@link Graphics2D}
 	 * object provided.
 	 * 
@@ -39,7 +44,8 @@ public abstract class Painter {
 	 * object provided.
 	 * 
 	 * <p>If the data hasn't changed and the width and height are the same as for the
-	 * previous call of this method, the object will be painted from the cache.</p>
+	 * previous call of this method, the object will be painted from the cache (unless
+	 * {@link #USE_CACHING} is false; in that case nothing will be cached).</p>
 	 * 
 	 * @param g2 The Graphics2D object to paint with.
 	 * @param x The x-coordinate of the upper-left corner.
@@ -53,15 +59,21 @@ public abstract class Painter {
 			return;
 		}
 		
-		if (cache == null || cache.getWidth() != width || cache.getHeight() != height) {
-			cache = new BufferedImage(width, height, BufferedImage.TYPE_4BYTE_ABGR);
-			Graphics2D gimg = (Graphics2D) cache.getGraphics();
-			gimg.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-			gimg.setRenderingHint(RenderingHints.KEY_ALPHA_INTERPOLATION, RenderingHints.VALUE_ALPHA_INTERPOLATION_QUALITY);
+		if (USE_CACHING) {
+			if (cache == null || cache.getWidth() != width || cache.getHeight() != height) {
+				cache = new BufferedImage(width, height, BufferedImage.TYPE_4BYTE_ABGR);
+				Graphics2D gimg = (Graphics2D) cache.getGraphics();
+				gimg.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+				gimg.setRenderingHint(RenderingHints.KEY_ALPHA_INTERPOLATION, RenderingHints.VALUE_ALPHA_INTERPOLATION_QUALITY);
+				
+				paintObject(gimg, width, height);
+			}
 			
-			paintObject(gimg, width, height);
+			g2.drawImage(cache, x, y, null);
+		} else {
+			g2.translate(x, y);
+			paintObject(g2, width, height);
+			g2.translate(-x, -y);
 		}
-		
-		g2.drawImage(cache, x, y, null);
 	}
 }
