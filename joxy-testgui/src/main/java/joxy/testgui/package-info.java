39d7323be3e3c7c71b31445d5215019968a7efc8
Willem Sonke <willem3141@users.sourceforge.net>
2013-08-17 12:52:36 +0200

    Fix compilation error due to wrong package statement

--- joxy-testgui/src/main/java/joxy/testgui/package-info.java
+++ joxy-testgui/src/main/java/joxy/testgui/package-info.java
@@ -4,4 +4,4 @@
  * <p>Note: these are not JUnit tests. There are some JUnit unit test cases in the
  * other packages, in the <code>test</code> source folder.</p>
  */
-package joxy.test;
+package joxy.testgui;
