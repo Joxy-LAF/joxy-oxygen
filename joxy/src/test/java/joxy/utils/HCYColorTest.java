82ea4bd7b60e1de758e3833d59c2a6c8b8fb1d8c
Thom Castermans <thomcastermans@users.sourceforge.net>
2014-01-03 16:26:21 +0100

    Added some more testcases to the HCYColorTest.

--- joxy/src/test/java/joxy/utils/HCYColorTest.java
+++ joxy/src/test/java/joxy/utils/HCYColorTest.java
@@ -29,9 +29,13 @@ import org.junit.Test;
 public class HCYColorTest {
 
     /**
+     * Delta for comparing floating point values.
+     */
+    private static final float DELTA = 0.0001f;
+    /**
      * Colours on which tests are performed.
      */
-    private Color[] testColors = new Color[] {
+    private static final Color[] TEST_COLORS = new Color[] {
             new Color(255, 255, 255), // white
             new Color(0, 0, 0), // black
             new Color(255, 0, 0), // red
@@ -42,11 +46,18 @@ public class HCYColorTest {
             new Color(128, 179, 255), // Oxygen colour: blue
             new Color(255, 0, 255) // colour that had negative hue
         };
+    /**
+     * HCY values to use when testing setters.
+     */
+    private static final float[][] TEST_HCY_VALUES = new float[][] {
+            new float[] {0.1f, 0.4f, 0.2f},
+            new float[] {0.8f, 0.1f, 0.5f}
+        };
     
     @Test
     public void testSameAfterConversion() {
         // Test for some colours if they are the same after conversion        
-        for (Color testColor : testColors) {
+        for (Color testColor : TEST_COLORS) {
             HCYColor testHCYColor = new HCYColor(testColor);
             Assert.assertEquals("Expected HCYColor.toColor() to return the " +
                     "same colour it was constructed from.", testColor,
@@ -57,21 +68,55 @@ public class HCYColorTest {
     @Test
     public void testGetters() {
         // Test if the getters return sane values
-        for (Color testColor : testColors) {
+        for (Color testColor : TEST_COLORS) {
             HCYColor testHCYColor = new HCYColor(testColor);
             Assert.assertTrue("Expected the hue value to be non-negative, but " +
                     "it was " + testHCYColor.getH() + " for input color " +
                     testColor + ".", 0.0f <= testHCYColor.getH());
-            Assert.assertTrue("Expected the hue value to be at most 1.0f.",
-                    testHCYColor.getH() <= 1.0f);
-            Assert.assertTrue("Expected the chroma value to be non-negative.",
-                    0.0f <= testHCYColor.getC());
-            Assert.assertTrue("Expected the chroma value to be at most 1.0f.",
-                    testHCYColor.getC() <= 1.0f);
-            Assert.assertTrue("Expected the luma value to be non-negative.",
-                    0.0f <= testHCYColor.getY());
-            Assert.assertTrue("Expected the luma value to be at most 1.0f.",
-                    testHCYColor.getY() <= 1.0f);
+            Assert.assertTrue("Expected the hue value to be at most 1.0f, but " +
+                    "it was " + testHCYColor.getH() + " for input color " +
+                    testColor + ".", testHCYColor.getH() <= 1.0f);
+            
+            Assert.assertTrue("Expected the chroma value to be non-negative, but " +
+                    "it was " + testHCYColor.getC() + " for input color " +
+                    testColor + ".", 0.0f <= testHCYColor.getC());
+            Assert.assertTrue("Expected the chroma value to be at most 1.0f, but " +
+                    "it was " + testHCYColor.getC() + " for input color " +
+                    testColor + ".", testHCYColor.getC() <= 1.0f);
+            
+            Assert.assertTrue("Expected the luma value to be non-negative, but " +
+                    "it was " + testHCYColor.getY() + " for input color " +
+                    testColor + ".", 0.0f <= testHCYColor.getY());
+            Assert.assertTrue("Expected the luma value to be at most 1.0f, but " +
+                    "it was " + testHCYColor.getY() + " for input color " +
+                    testColor + ".", testHCYColor.getY() <= 1.0f);
+        }
+    }
+    
+    @Test
+    public void testSetters() {
+        // Test if the setters really change the value and properly restore the color
+        // when set back for all test colours
+        for (Color testColor : TEST_COLORS) {
+            HCYColor testHCY = new HCYColor(testColor);
+            float[] oldHcyVals = new float[] {
+                    testHCY.getH(), testHCY.getC(), testHCY.getY()
+                };
+            for (float[] testHcyVals : TEST_HCY_VALUES) {
+                testHCY.setH(testHcyVals[0]);
+                Assert.assertEquals(testHCY.getH(), testHcyVals[0], DELTA);
+                
+                testHCY.setC(testHcyVals[1]);
+                Assert.assertEquals(testHCY.getC(), testHcyVals[1], DELTA);
+                
+                testHCY.setY(testHcyVals[2]);
+                Assert.assertEquals(testHCY.getY(), testHcyVals[2], DELTA);
+                
+                testHCY.setH(oldHcyVals[0]);
+                testHCY.setC(oldHcyVals[1]);
+                testHCY.setY(oldHcyVals[2]);
+                Assert.assertEquals(testColor, testHCY.toColor());
+            }
         }
     }
 }
